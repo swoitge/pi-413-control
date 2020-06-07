f4a1681e0b5df6d63d44fb6ee7637d7dde15d0f9
swoitge <66548513+swoitge@users.noreply.github.com>
2020-06-07 09:40:19 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /home/pi
 curl -o nodejs.tar.gz http://nodejs.org/dist/v9.9.0/node-v9.9.0-linux-armv6l.tar.gz
 tar -xzf nodejs.tar.gz
 sudo cp -r node-v9.9.0-linux-armv6l/* /usr/local/
