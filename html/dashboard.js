abc8db32edc6e366d0fd957cf60a5c349816a89b
stephan woitge <s.woitge@javengo.de>
2020-06-13 00:21:50 +0200

    ws returns send "result"

--- html/dashboard.js
+++ html/dashboard.js
@@ -6,7 +6,7 @@
   feather.replace();
 
   setInterval(function(){
-    api.rpi.requestI2C(0x41, function(msg){jQuery("#out_temp").html(msg.value);});
+    api.rpi.requestI2C(0x41, function(msg){jQuery("#out_temp").html(msg.result);});
   }, 2000);
 
 }())
