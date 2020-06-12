bd18c9c50de9da19e003bf5d54a1f8b8040dc8eb
stephan woitge <s.woitge@javengo.de>
2020-06-12 09:09:53 +0200

    removed range slider

--- html/app.js
+++ html/app.js
@@ -13,28 +13,15 @@ api.rpi.setServoRange = function(pin, range) {
   socket.send({msg:"setServoRange", pin, range});
 }
 
-// pwm range
-new Slider('#slider1', {
-  id:"slider-servo-1",
-  tooltip: 'always',
-  min: 0,
-  max: 2048,
-  //range: true,
-  value: 500
-}).on("slideStop", function(v){
-  console.log("on slideStop", arguments);
-  api.rpi.setServoRange(12, v);
-});
-
 var throttledSetPWM = _.throttle(function(v){
   console.log("on slideStop", arguments);
   api.rpi.setServoValue(12, v);
 }, 1000);
 
 //pwm value
-new Slider('#slider2', {
-  id:"slider-servo-2",
-  tooltip: 'always',
+new Slider('#slider1', {
+  id:"slider-servo-1",
+  //tooltip: 'always',
   min: 70,
   max: 400,
   //range: true,
