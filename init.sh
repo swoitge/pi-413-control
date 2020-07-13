b249b475d763059f79978088b0556f466ea35de5
stephan woitge <s.woitge@javengo.de>
2020-07-13 14:39:17 +0200

    node https download

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi
-curl -o nodejs.tar.gz http://nodejs.org/dist/v9.9.0/node-v9.9.0-linux-armv6l.tar.gz
+curl -o nodejs.tar.gz https://nodejs.org/dist/v9.9.0/node-v9.9.0-linux-armv6l.tar.gz
 tar -xzf nodejs.tar.gz
 sudo cp -r node-v9.9.0-linux-armv6l/* /usr/local/
 
