4d723ca5a05dc6b05c4dfc94f4dce9ac3f7a4cb6
swoitge <66548513+swoitge@users.noreply.github.com>
2021-02-01 12:30:49 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home/pi/meteor
+
+echo "start downloading $1"
+
+curl --user "$1" https://jenkins.yakaranda.com/job/live-view/lastSuccessfulBuild/artifact/_build/live-view.tar.gz --output live-view.tar.gz
+
+# extract
+tar -xzf live-view.tar.gz
+
+# build fibers
+cd /home/pi/meteor/bundle/programs/server/node_modules/fibers
+node ./build
+
+# add pigpio
+cd /home/pi/meteor/bundle/programs/server
+npm install rpio i2c-mpu6050
